4d5bd332c61dde09eba47400ac3d11467b19e382
mattmarnien <mgm215@gmail.com>
2020-03-17 22:18:40 -0400

    updated models for corss association, updated signup page, created frontend script for grabbing singup input and created api call to add user to DB, set up main page for basic query against games database to load some info, reconfigured connection to use config json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,25 +3,29 @@ const router = express.Router();
 
 // routes for api calls across unBoxed
 const db = require('../models')
-
+module.exports = function(router){
 router.post("/api/users", (req,res) =>{
-    User.create({
-        userName: req.body.userName,
+    console.log(req.body);
+    db.User.create({
+        username: req.body.username,
         email: req.body.email,
-        firstName: reqq.body.firstName,
-        lastName: req.body.lastName
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: req.body.password
+
     }).then(data => {
+        console.log("New user added")
         res.json(data);
     });
 
 });
 
 router.put("api/users/:id", (req,res) =>{
-    User.update({
-        userName: req.body.userName,
+    db.User.update({
+        username: req.body.username,
         email: req.body.email,
-        firstName: reqq.body.firstName,
-        lastName: req.body.lastName
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
     },
     {where: {
         id: req.params.id
@@ -32,15 +36,16 @@ router.put("api/users/:id", (req,res) =>{
 });
 
 router.delete("api/users/:id", (req, res) =>{
-    User.destroy({where: {id: req.params.id}}).then( data => {
+    db.User.destroy({where: {id: req.params.id}}).then( data => {
         res.json(data);
     });
 });
 
 router.put("/api/users/games/:id", async (req,res) => {
-     let thisUser = await User.findOne({where : {id = req.params.id}});
-    User.addGame(req.body.game)
+     let thisUser = await db.User.findOne({where : {id: req.params.id}});
+    db.User.addGame(req.body.game)
     .then(data => {
         res.json(data);
     })
 });
+}
