11fa9ede6651ebe9813aa952066e7a445dfa8632
mattmarnien <mgm215@gmail.com>
2020-03-22 18:21:23 -0400

    turning off force true

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ require("./routes/apiRoutes")(app, passport);
 require("./routes/htmlRoutes")(app, passport);
 
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
