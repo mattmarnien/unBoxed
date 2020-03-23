8e568de8f262128d68dce4e83dc42e4f40173121
mattmarnien <mgm215@gmail.com>
2020-03-23 10:46:01 -0400

    api key

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -195,7 +195,7 @@ module.exports = function (router) {
                 let thisUserZip = thisUser.zipcode;
                 let hasGamesArr = group.filter(user => user.games.length);
                 for (let i = 0; i < hasGamesArr.length; i++) {
-                    queryUrl = 'https://www.zipcodeapi.com/rest/5q6ubsVKpbj6rVavJtJmIV2m5GwMwtVXPEY8nWc33b5JML9KdbRPP1IiEcmLUdVy/distance.json/' + thisUserZip + '/' + hasGamesArr[i].dataValues.zipcode + '/mile';
+                    queryUrl = 'https://www.zipcodeapi.com/rest/NxbmU0XfrqhgZbnfvWZgwx44lpmTnAgkdcMwF5odkE0SxXcwTqta0gkfDkw0bvNm/distance.json/' + thisUserZip + '/' + hasGamesArr[i].dataValues.zipcode + '/mile';
                     
                     await axios.get(queryUrl).then(data => {
                         hasGamesArr[i].dataValues.distance = data.data.distance
